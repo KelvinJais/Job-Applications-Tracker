ee497403916cfbe9ffff38c72e17f23e2043a843
Kelvin Jaison <82458499+KelvinJais@users.noreply.github.com>
2024-06-25 18:29:53 +0400

    created applied and reject data

--- Getting_mails.py
+++ Getting_mails.py
@@ -2,7 +2,7 @@ from simplegmail import Gmail
 from simplegmail.query import construct_query
 import os
 from bs4 import BeautifulSoup
-
+import re
 gmail = Gmail()
 
 # Ensure the directories exist
@@ -12,8 +12,11 @@ os.makedirs("reject", exist_ok=True)
 # Function to extract text from an HTML file
 def extract_text_from_html(html_content):
     soup = BeautifulSoup(html_content, 'lxml')
-    text = soup.get_text()
-    return text
+    text = soup.get_text().strip()
+    clean_text = re.sub(r'\n+', '\n', text).strip()
+    clean_text = clean_text.replace('\t', ' ')
+    clean_text = ' '.join(clean_text.split())
+    return clean_text 
 
 def save_to_file(filename, content):
     with open(filename, 'a') as file:
@@ -30,10 +33,15 @@ messages = gmail.get_messages(query=construct_query(query_params))
 
 apply=1
 reject=1
-for message in messages[0:5]:
-    
-    string=message.sender+"\n"+message.subject+"\n"+extract_text_from_html(message.html)
+totalMessages=len(messages)
+count=1
+for message in messages:
+    print(f"{count}/{totalMessages}")
+    count+=1
+    string=message.id+"\n"+message.sender+"\n"+message.subject+"\n"+extract_text_from_html(message.html)
+    print()
     print(string)
+    print()
     option=input("Enter h for apply and l for reject:")
     if option=="h":
         save_to_file(os.path.join("apply", f"apply{apply}.txt"), string)
